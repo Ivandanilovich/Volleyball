bc0b138b6caf1b3fdd0b991918a5afaba340726f
Unknown <ivandanilovich99@gmail.com>
2019-04-28 21:11:26 +0700

    add ball

---
+++ Space Volleyball/Assets/scripts/BallController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class BallController : NetworkBehaviour
+{
+
+    public GameObject ball;
+    private Rigidbody rBall;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBall = ball.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rBall.transform.position.y <= 0.25)
+        {
+            //respawn
+        }
+      //  print("here");
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            print("inF");
+            rBall.useGravity = true;
+            rBall.transform.position = new Vector3(-2, 3, 0);
+        }
+    }
+}
--- Space Volleyball/Assets/scripts/MyNetworkManager.cs
+++ Space Volleyball/Assets/scripts/MyNetworkManager.cs
@@ -5,10 +5,19 @@ using UnityEngine.Networking;
 
 public class MyNetworkManager : NetworkManager
 {
+    public GameObject ballPrefab;
+    private GameObject ball;
     private short firstPlayerId = 0;
     void Start()
     {
     }
+
+    //[Command]
+    //[System.Obsolete]
+    //public void CmdSpawnBall()
+    //{
+    //    ball = Instantiate(ballPrefab, new Vector3(2, 3, 0), Quaternion.identity) as GameObject;
+    //}
     public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
     {
         //base.OnServerAddPlayer(conn, playerControllerId);
@@ -16,15 +25,18 @@ public class MyNetworkManager : NetworkManager
         if (firstPlayerId != 0)
         {
             player = Instantiate(playerPrefab, new Vector3(2, 0, 0), Quaternion.identity) as GameObject;
+          //  CmdSpawnBall();
         }
         else
         {
             player = Instantiate(playerPrefab, new Vector3(-2, 0, 0), Quaternion.identity) as GameObject;
         }
-        print("playerControllerId " + playerControllerId);
+    //    print("playerControllerId " + playerControllerId);
         NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
         firstPlayerId++;
     }
+
+  
     // Update is called once per frame
     void Update()
     {
