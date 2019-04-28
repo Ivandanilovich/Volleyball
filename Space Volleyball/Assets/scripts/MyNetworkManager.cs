a43307ea7b8fa29987675ed4f30f9d61d2020dbb
Unknown <ivandanilovich99@gmail.com>
2019-04-28 23:14:39 +0700

    another way to spawn ball

--- Space Volleyball/Assets/scripts/MyNetworkManager.cs
+++ Space Volleyball/Assets/scripts/MyNetworkManager.cs
@@ -5,41 +5,26 @@ using UnityEngine.Networking;
 
 public class MyNetworkManager : NetworkManager
 {
-    public GameObject ballPrefab;
-    private GameObject ball;
     private short firstPlayerId = 0;
-    void Start()
-    {
-    }
 
-    //[Command]
-    //[System.Obsolete]
-    //public void CmdSpawnBall()
-    //{
-    //    ball = Instantiate(ballPrefab, new Vector3(2, 3, 0), Quaternion.identity) as GameObject;
-    //}
+    private void InstantiateBall()
+    {
+        var ball = Instantiate(spawnPrefabs[0]);
+        NetworkServer.Spawn(ball);
+    }
     public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
     {
-        //base.OnServerAddPlayer(conn, playerControllerId);
         GameObject player;
         if (firstPlayerId != 0)
-        {
             player = Instantiate(playerPrefab, new Vector3(2, 0, 0), Quaternion.identity) as GameObject;
-          //  CmdSpawnBall();
-        }
         else
-        {
             player = Instantiate(playerPrefab, new Vector3(-2, 0, 0), Quaternion.identity) as GameObject;
-        }
-    //    print("playerControllerId " + playerControllerId);
         NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
         firstPlayerId++;
-    }
 
-  
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (NetworkServer.connections.Count == 2)
+        {
+            InstantiateBall();
+        }
     }
 }
